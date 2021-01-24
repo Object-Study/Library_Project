5596fe04727143e7ce3ba86ffa6c4f853a6311c1
neChak <34412522+cjy8922@users.noreply.github.com>
2021-01-24 20:16:55 +0900

    Add files via upload

--- Customer.java
+++ Customer.java
@@ -1,11 +1,20 @@
 public class Customer {
+    private Payment payment;
     private int mileage;
 
-    public Customer(int mileage) {
+    public Customer(Method method, int mileage, Book ... books) {
+        this.payment = new Payment(method, books);
         this.mileage = mileage;
     }
 
-    public void set_mileage(int mileage) {
+    public void get_pay() {
+        int total_pay = payment.final_payment();
+        int set_mileage = (int) Math.round(total_pay * 0.1);
+        set_mileage(set_mileage);
+
+    }
+
+    private void set_mileage(int mileage) {
         this.mileage += mileage;
     }
 }
